08db5b57b238b25bf80675f8019d8977100fb9aa
bangn <thebangnguyen@gmail.com>
2019-04-11 22:54:44 +1000

    [#11] Add health-check endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main (main) where
 
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
 import SstHrmsHaskellApi (someFunc)
 
+type HealthCheckApi = "health-check" :> Get '[JSON] String
+
+server :: Server HealthCheckApi
+server = return "ok"
+
+healthCheckApi :: Proxy HealthCheckApi
+healthCheckApi = Proxy
+
+app :: Application
+app = serve healthCheckApi server
 
 main :: IO ()
-main = someFunc
+main = run 8080 app
