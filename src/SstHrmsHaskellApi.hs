fdf04046d1c401267388f18c10b065c97f672952
bangn <thebangnguyen@gmail.com>
2019-04-18 00:00:29 +1000

    [#13]: Read port from PORT env var
    Default running on 3000 if PORT env var was not set

--- src/SstHrmsHaskellApi.hs
+++ src/SstHrmsHaskellApi.hs
@@ -7,9 +7,12 @@ module SstHrmsHaskellApi
 where
 
 import Api.HealthCheck (HealthCheckApi, healthCheckHandler)
+import Data.Maybe (fromMaybe)
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import System.Environment.Blank (getEnv)
+import Text.Read (read)
 
 type API = HealthCheckApi
 
@@ -23,4 +26,6 @@ app :: Application
 app = serve api server
 
 runApp :: IO ()
-runApp = run 8000 app
+runApp = do
+    mPort <- getEnv "PORT"
+    run (read $ fromMaybe "3000" mPort) app
